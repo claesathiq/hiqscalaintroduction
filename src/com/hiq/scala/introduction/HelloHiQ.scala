93c227fb723962a2cb27aabf5019a454f449f9fe
Claes Jonsson <claes@bbschk.com>
2013-11-25 12:25:07 +0100

    step 1

---
+++ src/com/hiq/scala/introduction/HelloHiQ.scala
@@ -0,0 +1,11 @@
+package com.hiq.scala.introduction
+
+/**
+ * Singleton instead of static
+ * Note that this creates an instance of an anonymous class
+ */
+object HelloHiQ {
+  def main(args: Array[String]) {
+    println("Hello HiQ!")
+  }
+}
